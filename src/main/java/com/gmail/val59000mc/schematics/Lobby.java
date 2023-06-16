Minecraft0122/UHC-Fork

commit 3fa0a19d073d16850fa6746053d575e5186a83bf

Fix some schematic handling problems
Fixes #66
Fixes #70

src/main/java/com/gmail/val59000mc/schematics/Lobby.java
@@ -32,11 +32,12 @@ public class Lobby extends Schematic {
 		}
 		// Build glass box
 		else {
-			int x = getLocation().getBlockX(), y=getLocation().getBlockY()+2, z=getLocation().getBlockZ();
+			getLocation().add(0, 2, 0);
+			int x = getLocation().getBlockX(), y=getLocation().getBlockY(), z=getLocation().getBlockZ();
 			World world = getLocation().getWorld();
-			for(int i = -width; i <= width; i++){
+			for(int i = -length; i <= length; i++){
 				for(int j = -height; j <= height; j++){
-					for(int k = -length ; k <= length ; k++){
+					for(int k = -width ; k <= width ; k++){
 						if(i == -10
 							|| i == 10
 							|| j == -3
@@ -55,12 +56,12 @@ public class Lobby extends Schematic {
 	}
 
 	public void destroyBoundingBox(){
-		int lobbyX = getLocation().getBlockX(), lobbyY = getLocation().getBlockY()+2, lobbyZ = getLocation().getBlockZ();
+		int lobbyX = getLocation().getBlockX(), lobbyY = getLocation().getBlockY(), lobbyZ = getLocation().getBlockZ();
 
 		World world = getLocation().getWorld();
-		for(int x = -width; x <= width; x++){
+		for(int x = -length; x <= length; x++){
 			for(int y = height; y >= -height; y--){
-				for(int z = -length ; z <= length ; z++){
+				for(int z = -width ; z <= width ; z++){
 					Block block = world.getBlockAt(lobbyX+x,lobbyY+y,lobbyZ+z);
 					if(!block.getType().equals(Material.AIR)){
 						block.setType(Material.AIR);

Support-WorldEdit-6/src/main/java/com/gmail/val59000mc/schematics/SchematicHandler8.java
@@ -31,6 +31,12 @@ public class SchematicHandler8{
 		ClipboardReader reader = format.getReader(new FileInputStream(schematic));
 		Clipboard clipboard = reader.read(world.getWorldData());
 
+		if (!clipboard.getRegion().contains(clipboard.getOrigin())) {
+			LOGGER.warning("Schematic origin is outside of bounds for " + path);
+			LOGGER.warning("The origin will be set to the center of the schematic");
+			clipboard.setOrigin(clipboard.getRegion().getCenter());
+		}
+
 		EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(world, -1);
 
 		Operation operation = new ClipboardHolder(clipboard, world.getWorldData())

Support-WorldEdit-7/src/main/java/com/gmail/val59000mc/schematics/SchematicHandler13.java
@@ -34,6 +34,12 @@ public class SchematicHandler13{
 		ClipboardReader reader = format.getReader(new FileInputStream(schematic));
 		Clipboard clipboard = reader.read();
 
+		if (!clipboard.getRegion().contains(clipboard.getOrigin())) {
+			LOGGER.warning("Schematic origin is outside of bounds for " + path);
+			LOGGER.warning("The origin will be set to the center of the schematic");
+			clipboard.setOrigin(clipboard.getRegion().getCenter().toBlockPoint());
+		}
+
 		EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(world, -1);
 
 		enableWatchdog(editSession);

src/main/java/com/gmail/val59000mc/schematics/DeathmatchArena.java
@@ -93,15 +93,15 @@ public class DeathmatchArena extends Schematic {
 		}
 		else{
 
-			int x = getLocation().getBlockX(),
-					y = getLocation().getBlockY(),
-					z = getLocation().getBlockZ();
+			int x = getLocation().getBlockX();
+			int y = getLocation().getBlockY();
+			int z = getLocation().getBlockZ();
 
 			LOGGER.info("Scanning schematic for arena teleport spots.");
 
-			for (int i = x - getWidth(); i < x + getWidth(); i++) {
+			for (int i = x - getLength(); i < x + getLength(); i++) {
 				for (int j = y - getHeight(); j < y + getHeight(); j++) {
-					for (int k = z - getLength(); k < z + getLength(); k++) {
+					for (int k = z - getWidth(); k < z + getWidth(); k++) {
 						Block block = getLocation().getWorld().getBlockAt(i, j, k);
 						if (block.getType().equals(spotMaterial) && hasAirOnTop(block)) {
 							spots.add(block.getLocation().clone().add(0.5, 1, 0.5));

commit fc89a5d3fb494f27ee671c7106e6bf07c8038d8d

Fix spawn location for glass box lobby
Regression introduced by 3fa0a19d

src/main/java/com/gmail/val59000mc/schematics/Lobby.java
@@ -17,7 +17,7 @@ public class Lobby extends Schematic {
 		// Dimensions for glass box
 		width = 10;
 		length = 10;
-		height = 3;
+		height = 6;
 	}
 
 	@Override
@@ -32,16 +32,15 @@ public class Lobby extends Schematic {
 		}
 		// Build glass box
 		else {
-			getLocation().add(0, 2, 0);
 			int x = getLocation().getBlockX(), y=getLocation().getBlockY(), z=getLocation().getBlockZ();
 			World world = getLocation().getWorld();
-			for(int i = -length; i <= length; i++){
-				for(int j = -height; j <= height; j++){
-					for(int k = -width ; k <= width ; k++){
+			for(int i = -10; i <= 10; i++){
+				for(int j = -1; j <= 5; j++){
+					for(int k = -10 ; k <= 10 ; k++){
 						if(i == -10
 							|| i == 10
-							|| j == -3
-							|| j == 3
+							|| j == -1
+							|| j == 5
 							|| k == -10
 							|| k == 10
 						){