Minecraft0122/UHC-Fork

commit db6a681720a3e5afd00161e4a32b81e252d61057

Delay setPlayerStartPlaying call until after teleportation
Fixes #67

src/main/java/com/gmail/val59000mc/players/PlayerManager.java
@@ -242,9 +242,6 @@ public class PlayerManager {
 					player.teleport(LocationUtils.withSameDirection(uhcPlayer.getStartingLocation(), player));
 					uhcPlayer.setHasBeenTeleportedToLocation(true);
 
-					// Remove lobby potion effects.
-					player.removePotionEffect(PotionEffectType.BLINDNESS);
-
 					// Call event
 					Bukkit.getPluginManager().callEvent(new PlayerStartsPlayingEvent(uhcPlayer));
 				}
@@ -331,7 +328,6 @@ public class PlayerManager {
 				{
 					player.removePotionEffect(effect.getType());
 				}
-				player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 999999, 1), false);
 				player.setGameMode(GameMode.SURVIVAL);
 				if(cfg.get(MainConfig.ENABLE_EXTRA_HALF_HEARTS)){
 					VersionUtils.getVersionUtils().setPlayerMaxHealth(player, 20+((double) cfg.get(MainConfig.EXTRA_HALF_HEARTS)));
@@ -484,7 +480,10 @@ public class PlayerManager {
 			}
 
 			for(UhcPlayer uhcPlayer : team.getMembers()){
-				gm.getPlayerManager().setPlayerStartPlaying(uhcPlayer);
+				uhcPlayer.setState(PlayerState.PLAYING);
+				try {
+					clearPlayerInventory(uhcPlayer.getPlayer());
+				} catch (UhcPlayerNotOnlineException ignored) {}
 			}
 
 			Bukkit.getScheduler().runTaskLater(UhcCore.getPlugin(), new TeleportPlayersThread(GameManager.getGameManager(), team), delayTeleportByTeam);
@@ -604,10 +603,6 @@ public class PlayerManager {
 	}
 
 	public void startWatchPlayerPlayingThread() {
-		for(Player player : Bukkit.getOnlinePlayers()){
-			player.removePotionEffect(PotionEffectType.BLINDNESS);
-		}
-
 		// Unfreeze players
 		for (UhcPlayer uhcPlayer : getPlayersList()){
 			uhcPlayer.releasePlayer();

src/main/java/com/gmail/val59000mc/threads/TeleportPlayersThread.java
@@ -11,7 +11,6 @@ import java.util.logging.Logger;
 
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class TeleportPlayersThread implements Runnable{
 
@@ -46,8 +45,8 @@ public class TeleportPlayersThread implements Runnable{
 
 			player.teleport(LocationUtils.withSameDirection(team.getStartingLocation(), player));
 
-			player.removePotionEffect(PotionEffectType.BLINDNESS);
-			player.setFireTicks(0);
+			gameManager.getPlayerManager().setPlayerStartPlaying(uhcPlayer);
+
 			uhcPlayer.setHasBeenTeleportedToLocation(true);
 		}
 	}

commit 99315c06898869eb9c2c9ffb43a83281a4121a65

Fix missing potion effects
Regression introduced by db6a6817

src/main/java/com/gmail/val59000mc/players/PlayerManager.java
@@ -236,11 +236,6 @@ public class PlayerManager {
 						}
 					}
 
-					// Apply start potion effect.
-					for(PotionEffect effect : GameManager.getGameManager().getConfig().get(MainConfig.POTION_EFFECT_ON_START)){
-						player.addPotionEffect(effect);
-					}
-
 					// Teleport player
 					UhcPlayer.teleport(player, LocationUtils.withSameDirection(uhcPlayer.getStartingLocation(), player));
 					uhcPlayer.setHasBeenTeleportedToLocation(true);
@@ -327,10 +322,13 @@ public class PlayerManager {
 				clearPlayerInventory(player);
 				player.setFireTicks(0);
 
-				for(PotionEffect effect : player.getActivePotionEffects())
-				{
+				for (PotionEffect effect : player.getActivePotionEffects()) {
 					player.removePotionEffect(effect.getType());
 				}
+				for (PotionEffect effect : cfg.get(MainConfig.POTION_EFFECT_ON_START)) {
+					player.addPotionEffect(effect);
+				}
+
 				player.setGameMode(GameMode.SURVIVAL);
 				if(cfg.get(MainConfig.ENABLE_EXTRA_HALF_HEARTS)){
 					VersionUtils.getVersionUtils().setPlayerMaxHealth(player, 20+((double) cfg.get(MainConfig.EXTRA_HALF_HEARTS)));

src/main/java/com/gmail/val59000mc/threads/TeleportPlayersThread.java
@@ -37,10 +37,6 @@ public class TeleportPlayersThread implements Runnable{
 
 			LOGGER.info("Teleporting " + player.getName() + " to " + team.getStartingLocation());
 
-			for(PotionEffect effect : gameManager.getConfig().get(MainConfig.POTION_EFFECT_ON_START)){
-				player.addPotionEffect(effect);
-			}
-
 			uhcPlayer.freezePlayer(team.getStartingLocation());
 
 			UhcPlayer.teleport(player, LocationUtils.withSameDirection(team.getStartingLocation(), player));